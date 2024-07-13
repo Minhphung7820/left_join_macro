2fdb120ca50d999699aef1e6ebcc807d257e13ae
Trương Minh Phung <minhphung485@gmail.com>
2024-07-13 09:26:06 +0700

    up

--- app/Macros/BillItemsMacro.php
+++ app/Macros/BillItemsMacro.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 Builder::macro('leftJoinItemsBill', function () {
   $this->leftJoin('bill_items', function ($join) {
@@ -9,5 +11,55 @@ Builder::macro('leftJoinItemsBill', function () {
       ->whereNotIn('bill_items.id', [2, 4]);
   })
     ->whereNotNull('bill_items.type');
+  return $this;
+});
+
+Builder::macro('leftJoinLatestSellOrPriceQuote', function ($contact_id) {
+
+  $sqlLatestPriceQuote = '
+  SELECT
+   *
+  FROM bills
+    WHERE contact_id = ?
+       AND type = "price_quote"
+    ORDER BY created_at
+    DESC LIMIT 1';
+
+  $sqlLatestSell = '
+  SELECT
+  *
+  FROM bills
+    WHERE contact_id = ?
+        AND type = "sell"
+        AND status = "approve"
+    ORDER BY created_at
+    DESC LIMIT 1';
+
+  $lLatestPriceQuote = DB::select($sqlLatestPriceQuote, [$contact_id]);
+  $latestSell = DB::select($sqlLatestSell, [$contact_id]);
+  $sqlConditionGetLatest = '';
+  if (!empty($lLatestPriceQuote) && !empty($latestSell)) {
+    $latestPriceQuoteCreatedAt = $lLatestPriceQuote[0]->created_at;
+    $latestSellCreatedAt = $latestSell[0]->created_at;
+    $priceQuoteTime = Carbon::parse($latestPriceQuoteCreatedAt);
+    $sellTime = Carbon::parse($latestSellCreatedAt);
+    $sqlConditionGetLatest = $priceQuoteTime->greaterThan($sellTime) ? $sqlLatestPriceQuote : $sqlLatestSell;
+  } elseif (empty($lLatestPriceQuote) && !empty($latestSell)) {
+    $sqlConditionGetLatest = $sqlLatestSell;
+  } elseif (!empty($lLatestPriceQuote) && empty($latestSell)) {
+    $sqlConditionGetLatest = $sqlLatestPriceQuote;
+  } else {
+    return [];
+  }
+  $arrayLeftJoinLatestCondition = [
+    DB::raw('(' . $sqlConditionGetLatest . ') as tbNewOrder'),
+    'tbNewOrder.id',
+    '=',
+    'bill_items.bill_id'
+  ];
+  $this->leftJoin(
+    ...$arrayLeftJoinLatestCondition
+  )->setBindings([$contact_id])
+    ->whereNotNull('tbNewOrder.id');
   return $this;
 });
--- routes/web.php
+++ routes/web.php
@@ -21,39 +21,13 @@ Route::get('/', function () {
     //
     $contact_id = '700';
 
-    $sqlLatestPriceQuote = 'SELECT * FROM bills WHERE contact_id = ? AND type = "price_quote" ORDER BY created_at DESC LIMIT 1';
-    $sqlLatestSell = 'SELECT * FROM bills WHERE contact_id = ? AND type = "sell" AND status = "approve" ORDER BY created_at DESC LIMIT 1';
-
-    $lLatestPriceQuote = DB::select($sqlLatestPriceQuote, [$contact_id]);
-    $latestSell = DB::select($sqlLatestSell, [$contact_id]);
-    $sqlConditionGetLatest = '';
-    if (!empty($lLatestPriceQuote) && !empty($latestSell)) {
-        $latestPriceQuoteCreatedAt = $lLatestPriceQuote[0]->created_at;
-        $latestSellCreatedAt = $latestSell[0]->created_at;
-        $priceQuoteTime = Carbon::parse($latestPriceQuoteCreatedAt);
-        $sellTime = Carbon::parse($latestSellCreatedAt);
-        $sqlConditionGetLatest = $priceQuoteTime->greaterThan($sellTime) ? $sqlLatestPriceQuote : $sqlLatestSell;
-    } elseif (empty($lLatestPriceQuote) && !empty($latestSell)) {
-        $sqlConditionGetLatest = $sqlLatestSell;
-    } elseif (!empty($lLatestPriceQuote) && empty($latestSell)) {
-        $sqlConditionGetLatest = $sqlLatestPriceQuote;
-    } else {
-        return response()->json([]);
-    }
-    $arrayLeftJoinLatestCondition = [
-        DB::raw('(' . $sqlConditionGetLatest . ') as tbNewOrder'),
-        'tbNewOrder.id',
-        '=',
-        'bill_items.bill_id'
-    ];
-    //
     $query = Product::leftJoin('bill_items', function ($join) {
         $join->on('bill_items.product_id', '=', 'products.id');
-    })->leftJoin(
-        ...$arrayLeftJoinLatestCondition
-    )->setBindings([$contact_id])
-        ->whereNotNull('tbNewOrder.id');
-
+    });
+    $leftJoinLatestSellOrPriceQuote = $query->leftJoinLatestSellOrPriceQuote($contact_id);
+    if (is_array($leftJoinLatestSellOrPriceQuote)) {
+        return response()->json([]);
+    }
     $data = $query->get();
 
     return response()->json($data);
