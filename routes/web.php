a1cb796a581d2bd0fe96fb59b195c79c15bd168a
Trương Minh Phung <minhphung485@gmail.com>
2024-07-21 12:03:48 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,37 @@ Route::get('/', function () {
     //
     $contact_id = '700';
 
-    $query = Product::leftJoin('bill_items', function ($join) {
-        $join->on('bill_items.product_id', '=', 'products.id');
-    });
-    $leftJoinLatestSellOrPriceQuote = $query->leftJoinLatestSellOrPriceQuote($contact_id);
-    if (is_array($leftJoinLatestSellOrPriceQuote)) {
-        return response()->json([]);
-    }
-    $data = $query->get();
+    // $query = Product::leftJoin('bill_items', function ($join) {
+    //     $join->on('bill_items.product_id', '=', 'products.id');
+    // });
+    // $leftJoinLatestSellOrPriceQuote = $query->leftJoinLatestSellOrPriceQuote($contact_id);
+    // if (is_array($leftJoinLatestSellOrPriceQuote)) {
+    //     return response()->json([]);
+    // }
+    // $data = $query->get();
+    // Lấy các sản phẩm đã mua của contact_id là 8
+    $products = Product::select('products.*', 'bill_items.amount', 'bills.type', 'bills.created_at')
+        ->join('bill_items', 'products.id', '=', 'bill_items.product_id')
+        ->join('bills', 'bill_items.bill_id', '=', 'bills.id')
+        ->where('bills.contact_id', $contact_id)
+        ->where(function ($query) {
+            $query->where(function ($query) {
+                $query->where('bills.type', 'sell');
+            })->orWhere(function ($query) {
+                $query->where('bills.type', 'price_quote');
+            });
+            $query->where('bills.status', 'approve');
+        })
+        ->whereIn('bill_items.id', function ($query) use ($contact_id) {
+            $query->select(DB::raw('MAX(bill_items.id)'))
+                ->from('bill_items')
+                ->join('bills', 'bill_items.bill_id', '=', 'bills.id')
+                ->where('bills.contact_id', $contact_id)
+                ->groupBy('bill_items.product_id');
+        })
+        ->orderByRaw("FIELD(bills.type, 'sell', 'price_quote')")
+        ->orderBy('bills.created_at', 'desc')
+        ->get();
 
-    return response()->json($data);
+    return response()->json($products);
 });
